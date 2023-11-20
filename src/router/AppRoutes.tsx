21d61087288c2f934188291708160bf6046aa5c2
Stefan Moldoveanu <mdv.stefan@gmail.com>
2023-11-20 11:32:13 +0100

    Added Configuration

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,9 +1,11 @@
 import {Navigate, Route, Routes} from "react-router-dom";
 import {DashboardScene} from "../scenes/DashboardScene.tsx";
+import {ConfigScene} from "../scenes/ConfigScene.tsx";
 
 export const AppRoutes: React.FC<unknown> = () => {
     return <Routes>
         <Route path="/" element={<Navigate to="/dashboard"/>}/>
         <Route path="/dashboard" element={<DashboardScene/>}/>
+        <Route path="/configuration" element={<ConfigScene/>}/>
     </Routes>
 }
